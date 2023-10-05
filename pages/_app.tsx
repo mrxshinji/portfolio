ef15d0630b778656eb0e888deacdbac90f04438a
mrxshinji <95459307+mrxshinji@users.noreply.github.com>
2023-10-05 13:23:55 +0800

    update preload img

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/layout";
 import Head from "next/head";
+import projects from "lib/pastWorkToUse.json";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>mrxShinji`s PortFolio</title>
         <meta name='description' content='Portfolio of mrxShinji' />
         <link rel='icon' href='/favicon.ico' />
+        {projects.map((project, idx) => {
+          return (
+            <link key={idx} rel='preload' href={project.link} as='image' />
+          )
+        })}
       </Head>
       <Layout>
         <Component {...pageProps} />
